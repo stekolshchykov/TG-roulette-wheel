60c449a8b878c9d5e26ad09ec62d9f40d88ba059
mk <v.a.stekolschikov@gmail.com>
2025-01-07 19:57:24 +0000

    refactoring

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,8 @@ export default {
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/layout/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/ui/**/*.{js,ts,jsx,tsx,mdx}"
+        "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/modals/**/*.{js,ts,jsx,tsx,mdx}"
     ],
     theme: {
         extend: {
