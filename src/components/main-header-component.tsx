4ce80c753a6ebe01cf9f0ed75fb39d30b93fc922
mk <v.a.stekolschikov@gmail.com>
2025-01-07 19:39:21 +0000

    added coupon page

--- src/components/main-header-component.tsx
+++ src/components/main-header-component.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LinkToPage = (props: { link: string; title: string; icon: string; isActive: boolean }) => {
     return (
         <li className={`text-white ${props.isActive ? "bg-main-accent" : ""} flex gap-2 w-full px-4 py-3 rounded-2xl`}>
-            <Image src={props.icon} alt="icon" width={24} height={24}/>
-            <span className="truncate">{props.title}</span>
+            <Link href={props?.link} className={"flex gap-2"}>
+                <Image src={props.icon} alt="icon" width={24} height={24}/>
+                <span className="truncate">{props.title}</span>
+            </Link>
         </li>
     );
 };
@@ -13,7 +16,7 @@ const LinkToPage = (props: { link: string; title: string; icon: string; isActive
 const MainHeaderComponent = () => {
     return (
         <ul className="flex text-white gap-3">
-            <LinkToPage link={"#"} icon={"/icon/coupon.svg"} title={"Мои купоны"} isActive={true}/>
+            <LinkToPage link={"/coupons"} icon={"/icon/coupon.svg"} title={"Мои купоны"} isActive={true}/>
             <LinkToPage link={"#"} icon={"/icon/question.svg"} title={"Как играть?"} isActive={false}/>
         </ul>
     );
